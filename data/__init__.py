d473ccc0fcf25778fbfedacb50c5ffe4abf88f53
Zhi <yfwz100@yeah.net>
2016-04-30 22:30:30 +0800

    Modify the data loading function.

--- data/__init__.py
+++ data/__init__.py
@@ -1,42 +1,45 @@
 # coding: utf-8
 
-import pandas as pd
+from pandas import *
 from datetime import datetime as dt
 
+def koubei(parsed_date=False):
+    kb = read_csv(
+        'data/ijcai2016_koubei_train', header=None, names=['user_id', 'merchant_id', 'location_id', 'timestamp']
+        #   , dtype=str
+        #   , parse_dates=[3]
+        #   , nrows=10
+    )
+    if parsed_date:
+        train['timestamp'] = train.timestamp.map(lambda d: dt.strptime(d, '%Y%m%d'))
+    return kb
 
-# read the train log.
-train = pd.read_csv(
-    'data/ijcai2016_koubei_train'
-    , header=None, names=['user_id', 'merchant_id', 'location_id', 'timestamp']
-    , dtype=str
-#   , parse_dates=[3]
-#   , nrows=10
-)
-train['timestamp'] = train.timestamp.map(lambda d: dt.strptime(d, '%Y%m%d'))
+def taobao(parsed_date=False):
+    # read the taobao data.
+    taobao = read_csv(
+        'data/ijcai2016_taobao', header=None, names=['user_id', 'seller_id', 'item_id', 'category_id', 'action_id', 'timestamp']
+        #   , dtype=str
+        #   , parse_dates=[5]
+        #   , nrows=10
+    )
+    if parsed_date:
+        taobao['timestamp'] = taobao.timestamp.map(lambda d: dt.strptime(d, '%Y%m%d'))
+    return taobao
 
+def merchant(parse_location=False):
+    # read the merchant's info
+    merchant = read_csv(
+        'data/ijcai2016_merchant_info'
+        , header=None, names=['merchant_id', 'budget', 'locations']
+    )
+    if parse_location:
+        merchant['locations'] = merchant.locations.str.split(':')
+    return merchant
 
-# read the taobao data.
-taobao = pd.read_csv(
-    'data/ijcai2016_taobao'
-    , header=None, names=['user_id', 'seller_id', 'item_id', 'category_id', 'action_id', 'timestamp']
-    , dtype=str
-#   , parse_dates=[5]
-#   , nrows=10
-)
-taobao['timestamp'] = taobao.timestamp.map(lambda d: dt.strptime(d, '%Y%m%d'))
-
-
-# read the merchant's info
-merchant = pd.read_csv(
-    'data/ijcai2016_merchant_info'
-    , header=None, names=['merchant_id', 'budget', 'locations']
-)
-merchant['locations'] = merchant.locations.str.split(':')
-
-
-# read the test set to predict.
-pred = pd.read_csv(
-    'data/ijcai2016_koubei_test'
-    , header=None, names=['user_id', 'location_id']
-)
+def pred():
+    # read the test set to predict.
+    pred = read_csv(
+        'data/ijcai2016_koubei_test', header=None, names=['user_id', 'location_id']
+    )
+    return pred
 
